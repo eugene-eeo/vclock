38655a6e5d262aee5871cb2711da10872579a845
Eeo Jun <packwolf58@gmail.com>
2016-05-30 18:31:07 +0800

    Initial commit

---
+++ vclock.py
@@ -0,0 +1,25 @@
+from functools import cmp_to_key, partial
+
+
+def from_size(n):
+    return (0,) * n
+
+
+def merge(a, b):
+    return tuple(max(j, k) for j, k in zip(a, b))
+
+
+def compare(a, b):
+    greater = False
+    smaller = False
+    for j, k in zip(a, b):
+        greater |= j > k
+        smaller |= j < k
+    return greater - smaller
+
+
+sort = partial(sorted, key=cmp_to_key(compare))
+
+
+def is_concurrent(a, b):
+    return compare(a, b) == 0
