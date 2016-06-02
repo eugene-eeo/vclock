303c7d67ff0b56f4120333f37d8adc35102f6aa7
Eeo Jun <packwolf58@gmail.com>
2016-06-02 21:09:50 +0800

    Add docs

--- vclock.py
+++ vclock.py
@@ -1,3 +1,13 @@
+"""
+    vclock
+    ~~~~~~
+
+    Functions for manipulating vector clocks.
+
+    :copyright: (C) 2016 by Eeo Jun
+    :license: MIT, see LICENSE for details.
+"""
+
 import sys
 import itertools
 from functools import cmp_to_key, partial
@@ -8,14 +18,27 @@ if sys.version_info[0] == 2:
 
 
 def from_size(n):
+    """
+    Constructs a zeroed, *n* sized vector clock.
+    """
     return (0,) * n
 
 
 def merge(a, b):
+    """
+    Given two clocks, return a new clock with all
+    values greater or equal to those of the merged
+    clocks.
+    """
     return tuple(map(max, zip(a, b)))
 
 
 def compare(a, b):
+    """
+    Compares two vector clocks, returns -1 if ``a < b``,
+    1 if ``a > b`` else 0 for concurrent events
+    or identical values.
+    """
     gt = False
     lt = False
     for j, k in zip(a, b):
@@ -27,6 +50,12 @@ def compare(a, b):
 
 
 def sort(xs, key=None, reverse=False):
+    """
+    Sort iterable *xs* using the ``vclock.compare``
+    algorithm, optionally with a *key* function and
+    whether to *reverse* the sorting (defaults to
+    ascending order).
+    """
     cmpfunc = (
             compare if key is None else
             lambda a, b: compare(key(a), key(b))
@@ -36,10 +65,17 @@ def sort(xs, key=None, reverse=False):
 
 
 def is_concurrent(a, b):
+    """
+    Returns whether the given clocks are concurrent.
+    They must not be equal in value.
+    """
     return (a != b) and compare(a, b) == 0
 
 
 def increment(clock, index):
+    """
+    Increment the clock at *index*.
+    """
     return clock[:index] \
             + (clock[index] + 1,) \
             + clock[index+1:]
